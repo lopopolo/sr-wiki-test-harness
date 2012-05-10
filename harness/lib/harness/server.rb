6cdbda1f8bbc5bce26b32237fcbc71f26c8e16ce
Ryan Lopopolo <rjl@hyperbo.la>
2012-05-10 15:31:45 -0400

    Make the harness respond to both get and post requests

--- harness/lib/harness/server.rb
+++ harness/lib/harness/server.rb
@@ -6,7 +6,12 @@ require "harness/datastore"
 
 module Harness
   class Server < Sinatra::Base
-    get '/next_page/:seq_list' do
+    def self.get_or_post(path, opts={}, &block)
+      get(path, opts, &block)
+      post(path, opts, &block)
+    end
+
+    get_or_post '/next_page/:seq_list' do
       response = {}
       params[:seq_list].split(",").map { |seq| seq.to_i }.each do |seq|
         response[seq] = Harness::datastore.get_page_seq seq
@@ -14,7 +19,7 @@ module Harness
       response.to_json
     end
 
-    get '/next_rev/:seq_list' do
+    get_or_post '/next_rev/:seq_list' do
       response = {}
       params[:seq_list].split(",").map { |seq| seq.to_i }.each do |seq|
         response[seq] = Harness::datastore.get_rev_seq seq
@@ -23,12 +28,12 @@ module Harness
       response.to_json
     end
 
-    get "/num_revs" do
+    get_or_post "/num_revs" do
       { num_revs: Harness::datastore.get_num_revs }.to_json
     end
 
 
-    get %r{/(index)?} do
+    get_or_post %r{/(index)?} do
       "This server is a test harness for sr.<br/>" +
         'See <a href="http://wiki.hyperbo.la/sr">wiki.hyperbo.la/sr</a> for more info.'
     end
